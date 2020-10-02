4c7d17b2d70648b76b0253041268c585e824d259
Kelly Nagy Bertels <kelly@brickfieldlabs.ie>
2020-10-02 11:23:03 +0100

    Basic info necessary to install a blank new plugin

---
+++ staffmanager/index.php
@@ -0,0 +1,47 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//###References####: 
+/* Reference for the data Bellow: Book: "moodle 3.1 LTS modules development by Tomasz Muras
+//$CFG - defined in config.php as stdClass6 contains various Moodle configuration variables.
+Some will be defined in config.php but most will be fetched from database, mdl_config table
+during initialization (function initialise_cfg() from lib/setuplib.php does the job).
+//$DB - provides access to database API methods, currently supported drivers are pgsql, mariadb,
+mysqli, mssql, sqlsrv and ocilib - all defined in lib/dml/*_native_moodle_database.php
+//$USER - access to current user’s information. Just like with $SESSION, it’s initialized by
+core\session\manager and stored in $_SESSION[‘USER’]. */
+
+//the config file is two folders above this one, 
+require_once '../../config.php';
+//Those are the global setting that we will use from the config file
+global $USER,$DB,$CFG;
+
+//using the variable $PAGE moodle knows what an url is and where it is 
+$PAGE->set_url('/local/staffmanager/index.php');
+$PAGE->set_context(context_system::instance());
+
+require_login();
+
+//the title of this page:it is using the language file to set this value. 
+$strpagetitle = get_string('staffmanager', 'local_staffmanager');
+//this will use the strpagetitle to set the title inside the page, in moodle this is how the title is set inside the page. 
+$PAGE->set_title($strpagetitle);
+
+//this is outputing the standard header from moodle
+echo $OUTPUT->header();
+
+echo $OUTPUT->footer();
+
